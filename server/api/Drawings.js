670cc4b021fc50c2b4c3fcda755a81e28a300f8f
dahn0209 <dahn0209@gmail.com>
2021-11-24 00:31:14 -0500

    changes to error

--- server/api/Drawings.js
+++ server/api/Drawings.js
@@ -1,15 +1,29 @@
 const router = require('express').Router()
-const {Drawing} = require('../db/models')
+const {Drawing, User} = require('../db/models')
 
-// <-- assumes main route to products set up with app.use in index.js -->  //
+// <-- assumes main route to drawings set up with app.use in index.js -->  //
 
+/////finding each signedIn user//////
+
+// async function checkUser(req, res, next) {
+//   // checks if someone is logged in
+//   if (req.session.passport) {
+//     // this userId is only accessible if someone is logged in
+//     const userId = req.session.passport.user
+//     const homeDog= await User.findByPk(userId);
+//     console.log('homeDog==>',homeDog);
+
+//   }
+// }
+
+/////find all drawings for signedIn User/////
 router.get('/', async (req, res, next) => {
   try {
-    const allDrawings = await Drawing.findAll()
-    allDrawings.map(drawing => {
-      return drawing
-    })
+    const userId = req.session.passport.user
+    const allDrawings = await Drawing.findAll({where: {userId: userId}})
+    console.log('userId in Drawing.js==>', userId)
     res.json(allDrawings)
+    console.log('allDrawings api/drawings.js===>', allDrawings)
   } catch (err) {
     next(err)
   }
@@ -21,11 +35,58 @@ router.get('/:drawingId', async (req, res, next) => {
 
     if (drawing) {
       res.json(drawing)
+      console.log('getIndividiaulDrawing,drawing')
     } else {
       res.status(404).send('drawing not found')
     }
   } catch (err) {
     next(err)
+  }
+})
+
+// /api/drawings
+//////create drawing for signedIn user
+router.post('/', async (req, res, next) => {
+  const userId = req.session.passport.user
+
+  try {
+    const drawing = await Drawing.create(req.body)
+    console.log('drawing post4==>', drawing)
+
+    res.send(drawing)
+    console.log('afterdrawing post4===>', drawing)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// /api/drawings/:id
+//////update drawing for signedIn user
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const drawingId = req.params.id
+    const drawing = await Drawing.findByPk(drawingId)
+    console.log('drawingPut===>', drawing)
+    res.send(await drawing.update(req.body))
+  } catch (error) {
+    next(error)
+  }
+})
+
+// /api/drawings/:id
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const drawingId = req.params.id
+    const drawing = await Drawing.findByPk(drawingId)
+
+    if (!drawing) {
+      res.sendStatus(404)
+    }
+    await drawing.destroy()
+    res.send(drawing)
+  } catch (error) {
+    next(error)
   }
 })
 
